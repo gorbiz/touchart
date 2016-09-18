a043d580dd24913d7d48eecbeea1a65250b55a92
Karl Svartholm <gorbiz@gmail.com>
2016-09-18 23:56:36 +0200

    squares rotating EVERYWHERE! (using Raphaël/SVG)

--- art.js
+++ art.js
@@ -1,14 +1,13 @@
-function drawDot(ctx, x, y, color, size) {
+var rotate = Raphael.animation({transform: "r180"}, 1500).repeat(Infinity);
+
+function drawDot(paper, x, y, color, size) {
   var c = Object.assign({r:255, g:255, b:255, a:1}, color || {});
   size = size || 5;
 
-  // Select a fill style
-  ctx.fillStyle = `rgba(${c.r}, ${c.g}, ${c.b}, ${c.a})`;
-  // Draw a filled circle
-  ctx.beginPath();
-  ctx.arc(x, y, size, 0, Math.PI*2, true);
-  ctx.closePath();
-  ctx.fill();
+  var shape = paper.rect(x-size/2, y-size/2, size, size);
+  shape.attr('fill', `rgba(${c.r}, ${c.g}, ${c.b}, ${c.a})`);
+  shape.attr('stroke', 'none');
+  shape.animate(rotate);
 }
 
 function randomColor() {
@@ -22,40 +21,35 @@ function randomColor() {
 function getTouchPos(e) {
   return Array.from(e.touches).map(function(t) {
     return {
-      x: t.pageX - t.target.offsetLeft,
-      y: t.pageY - t.target.offsetTop
+      x: t.pageX,
+      y: t.pageY
     };
   });
 }
 
-function draw(e, ctx, opt) {
-  e.preventDefault();
-  var color = Object.assign(randomColor(), {a: opt.alpha});
-  var size = Math.random()*opt.size;
-  getTouchPos(e).map(function(pos) {
-    drawDot(ctx, pos.x,     pos.y,     color, size);
-    drawDot(ctx, pos.y,     pos.x,     color, size);
-    drawDot(ctx, dim-pos.x, pos.y,     color, size);
-    drawDot(ctx, dim-pos.y, pos.x,     color, size);
-    drawDot(ctx, pos.x,     dim-pos.y, color, size);
-    drawDot(ctx, pos.y,     dim-pos.x, color, size);
-    drawDot(ctx, dim-pos.x, dim-pos.y, color, size);
-    drawDot(ctx, dim-pos.y, dim-pos.x, color, size);
-  })
-}
 
+
+var fullWidth  = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+var fullHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+
+var parentElement = document.getElementById('sketchpad');
+var paper = Raphael(parentElement, fullWidth, fullHeight);
 var opt = {
-  size:  (window.location.search.match(/[\?&]s(ize)?=([\d\.]+)/) || [null,null,500])[2],
+  size:  (window.location.search.match(/[\?&]s(ize)?=([\d\.]+)/) || [null,null,50])[2],
   alpha: (window.location.search.match(/[\?&]a(lpha)?=([\d\.]+)/) || [null,null,.05])[2]
 };
 
-var canvas = document.getElementById('sketchpad');
-var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-var dim = Math.min(w, h);
-canvas.setAttribute('width', dim);
-canvas.setAttribute('height', dim);
+var svg = document.getElementsByTagName('svg')[0];
+svg.addEventListener('touchstart', draw, false);
+svg.addEventListener('touchmove',  draw, false);
 
-var ctx = canvas.getContext('2d');
-canvas.addEventListener('touchstart', function(e) { draw(e, ctx, opt); }, false);
-canvas.addEventListener('touchmove', function(e) { draw(e, ctx, opt); }, false);
+var limit = 0;
+function draw(e) {
+  e.preventDefault();
+  if (++limit%2 != 1) return;
+  var color = Object.assign(randomColor(), {a: opt.alpha});
+  var size = opt.size;
+  getTouchPos(e).map(function(pos) {
+    drawDot(paper, pos.x, pos.y, color, size);
+  });
+}
